aece2c7c11cd7bc6822133ceaea0bac8c679dcef
Toyro98 <teddymolin1@gmail.com>
2022-09-15 16:50:11 +0200

    Added simple camera and movement script

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public sealed class PlayerCamera : MonoBehaviour
+{
+    public float sensitivity = 2f;
+    private float _yaw;
+    private float _pitch;
+    private float _roll;
+    private float _angle = 0f;
+    public Transform playerBody;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // Gets the mouse input
+        _yaw = Input.GetAxis("Mouse X") * sensitivity; // left right
+        _pitch = Input.GetAxis("Mouse Y") * sensitivity; // up down
+
+        // Locks the angle
+        _angle -= _pitch;
+        _angle = Mathf.Clamp(_angle, -90f, 90f);
+
+        // Rotate body and camera
+        transform.localRotation = Quaternion.Euler(_angle, 0f, 0f);
+        playerBody.transform.Rotate(Vector3.up * _yaw);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public sealed class PlayerMovement : MonoBehaviour
+{
+    private const float GRAVITY = -9.81f;
+    
+    public float speed = 1f;
+    public float jumpHeight = 1f;
+    public bool isGrounded = false;
+    
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public Vector3 groundDistance = new Vector3(0f, 0.5f, 0f);
+
+    private Vector3 _velocity;
+    private Vector3 _movement;
+
+    private void Update()
+    {
+        if (IsGrounded() && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        _movement = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        _velocity.y += GRAVITY * Time.deltaTime;
+
+        controller.Move(speed * Time.deltaTime * _movement);
+        controller.Move(_velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            _velocity.y = Mathf.Sqrt(jumpHeight * -2f * GRAVITY);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckBox(groundCheck.position, groundDistance, Quaternion.identity, groundLayer);
+    }
+}
