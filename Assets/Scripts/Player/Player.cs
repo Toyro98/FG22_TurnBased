2c81cb8d41267499409c74475196fdbe6fa325da
Toyro98 <teddymolin1@gmail.com>
2022-09-22 17:17:01 +0200

    Update Player.cs
    Added scripts to the Player.cs so I don't have to get the component everytime

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,30 +3,39 @@ using TMPro;
 
 public sealed class Player : MonoBehaviour, IDamageable
 {
-    public new string name = "Nameless";
+    public new string name = "Name";
     public int health = 100;
 
-    // Used to display player's name and health
+    [Header("UI")]
     [SerializeField] private Camera _mainCamera;
-    [SerializeField] private RectTransform _canvas;
-    public TMP_Text nameText;
-    public TMP_Text healthText;
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] private TextMeshProUGUI _healthText;
+
+    [Header("Player")]
+    [SerializeField] private MeshRenderer _playerMesh;
+    [SerializeField] private PlayerMovement _playerMovement;
+    [SerializeField] private Camera _playerCamera;
+    [SerializeField] private PlayerCamera _playerCameraScript;
 
     private void Start()
     {
         _mainCamera = Camera.main;
-        _canvas = GetComponentInChildren<RectTransform>();
+        _canvas = GetComponentInChildren<Canvas>();
 
-        TMP_Text[] texts = _canvas.GetComponentsInChildren<TMP_Text>();
-        nameText = texts[0];
-        healthText = texts[1];
+        TextMeshProUGUI[] texts = _canvas.GetComponentsInChildren<TextMeshProUGUI>();
+        _nameText = texts[0];
+        _healthText = texts[1];
+
+        _nameText.text = name;
+        _healthText.text = health.ToString();
     }
 
     public void Update()
     {
         // Look at the main camera
-        Quaternion rotation = _mainCamera.transform.rotation;
-        _canvas.LookAt(_canvas.transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+        Quaternion rotation = Camera.main.transform.rotation;
+        _canvas.transform.LookAt(_canvas.transform.position + rotation * Vector3.forward, rotation * Vector3.up);
 
         // Temp
         if (Input.GetKeyDown(KeyCode.T))
@@ -45,9 +54,19 @@ public sealed class Player : MonoBehaviour, IDamageable
         }
         else
         {
-            healthText.text = health.ToString();
+            _healthText.text = health.ToString();
 
             // Todo: Display how much damage you took and then have text move up and fade away
         }
+    }
+
+    public void Toggle()
+    {
+        _canvas.enabled = !_canvas.enabled;
+
+        _playerMovement.enabled = !_playerMovement.enabled;
+        _playerCameraScript.enabled = !_playerCameraScript.enabled;
+        _playerCamera.enabled = !_playerCamera.enabled;
+        _playerMesh.enabled = !_playerMesh.enabled;
     }
 }
