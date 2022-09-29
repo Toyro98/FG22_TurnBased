22dc058fa6b3059e2049848157929958618ae63c
Toyro98 <teddymolin1@gmail.com>
2022-09-29 16:05:25 +0200

    Update Rocket.cs

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Rocket : MonoBehaviour
 {
     private void Start()
     {
-        Destroy(gameObject, 5f);   
+        GetComponent<Rigidbody>().AddForce(transform.forward * 25, ForceMode.Impulse);    
+        Destroy(gameObject, 5f);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject.TryGetComponent<IDamageable>(out var target))
+        {
+            target.TakeDamage(1);
+        }
+
+        Destroy(gameObject);
     }
 }
