c4cd1a603991fb64bc7576cedfd43586b38120b8
Toyro98 <teddymolin1@gmail.com>
2022-10-05 23:34:14 +0200

    Update PlayerMovement.cs
    Fixed movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,59 +3,58 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public sealed class PlayerMovement : MonoBehaviour
 {
-    private const float GRAVITY = -9.81f * 2f;
+    private const float GRAVITY = -9.81f;
     
-    public float speed = 3f;
-    public float jumpHeight = 1f;
+    public float speed = 4f;
+    public float jumpHeight = 2.5f;
     public bool canMoveAround = false;
     
-    private CharacterController _controller;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-    public Vector3 groundDistance;
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundDistance = 0.3f;
 
-    private Vector3 _velocity;
-    private Vector3 _movement;
-
-    private void Start()
-    {
-        _controller = GetComponent<CharacterController>();
-    }
+    [SerializeField] private Vector3 _velocity;
+    [SerializeField] private Vector3 _movement;
 
     private void Update()
     {
+        if (GameManager.IsGamePaused) 
+            return;
+
         Movement();
     }
 
     private void Movement()
     {
-        if (IsGrounded() && _velocity.y < 0)
+        bool isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundLayer);
+
+        if (isGrounded && _velocity.y < 0)
         {
             _velocity.y = -2f;
         }
 
-        if (canMoveAround && !GameManager.IsGamePaused)
+        if (canMoveAround)
         {
+            // Input
             _movement = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
-            _velocity.y += GRAVITY * Time.deltaTime;
-
             _controller.Move(speed * Time.deltaTime * _movement);
+
+            // Gravity
+            _velocity.y += GRAVITY * Time.deltaTime;
             _controller.Move(_velocity * Time.deltaTime);
 
-            if (Input.GetButtonDown("Jump") && IsGrounded())
+            // Jumping
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             {
                 _velocity.y = Mathf.Sqrt(jumpHeight * -2f * GRAVITY);
             }
         }
         else
         {
+            // Apply gravity even if we can't move
             _velocity.y += GRAVITY * Time.deltaTime;
             _controller.Move(_velocity * Time.deltaTime);
         }
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics.CheckBox(groundCheck.position, groundDistance, Quaternion.identity, groundLayer);
     }
 }
