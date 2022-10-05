5ef2e84160c1eda4b1c556a9d57e118287b8c034
Toyro98 <teddymolin1@gmail.com>
2022-10-05 04:16:04 +0200

    Implemented Hitpoint

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int damage; 
+    public Camera cameraLookAt;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private TMPro.TextMeshProUGUI healthText;
+
+    private void Start()
+    {
+        healthText.text = "-" + damage;
+        animator.Play("Health");
+        Destroy(gameObject, animator.GetCurrentAnimatorClipInfo(0).Length);
+    }
+
+    private void Update()
+    {
+        Quaternion rotation = cameraLookAt.transform.rotation;
+        canvas.transform.LookAt(canvas.transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+    }
+}
