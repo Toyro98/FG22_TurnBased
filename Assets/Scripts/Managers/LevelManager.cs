24a7a12cbce2c881fdd316b5b5d478194b0b964e
Toyro98 <teddymolin1@gmail.com>
2022-10-05 04:11:46 +0200

    Update LevelManager.cs
    Added timescale and updating the IsGamePaused just to be sure. Might be removed later

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -6,11 +6,15 @@ public class LevelManager : MonoBehaviour
     public void GoToMainMenu()
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+        GameManager.IsGamePaused = false;
     }
 
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+        GameManager.IsGamePaused = false;
     }
 
     public void StartLevel()
